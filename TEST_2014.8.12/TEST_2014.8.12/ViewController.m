4a9fbbce8f933121b224f0cd9eb89920af07685e
姜宇 <jiangyu@jiangyudeMacBook-Pro.local>
2014-08-15 11:53:02 +0800

    测试

--- TEST_2014.8.12/TEST_2014.8.12/ViewController.m
+++ TEST_2014.8.12/TEST_2014.8.12/ViewController.m
@@ -20,6 +20,10 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewWillAppear{
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
